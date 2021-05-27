b6fbb8b120b556ae1a07876d2ada9566bfe4735f
Walace Nascimento <wlacens@hotmail.com>
2021-05-27 14:54:39 -0300

    Adiciona no bloco 4 a aula 1

---
+++ Fundamentos/bloco-4/aula-1/parte1.js
@@ -0,0 +1,16 @@
+let age = 20;
+// console.log(age);
+
+// let birtYear = 1981;
+// console.log(name);
+// console.log(birthCity);
+// console.log(birtYear);
+console.log(typeof age);
+
+
+let salary = 3500;
+console.log(salary + salary);
+console.log(salary - salary);
+console.log(salary * salary);
+console.log(salary / salary);
+console.log(3 ** 3);
