900d21b413e769d57999b61672e746713046f09b
Walace Nascimento <wlacens@hotmail.com>
2021-06-22 13:49:28 -0300

    Resolvendo os exercicios parte 1

--- fundamentos/bloco-7/aula-3/4exercico1.js
+++ fundamentos/bloco-7/aula-3/4exercico1.js
@@ -1,3 +1,9 @@
+// 1-0 Testa se sum é uma função.
+// 1-1 Teste se o retorno de sum(4, 5) é 9
+// 1-2 Teste se o retorno de sum(0, 0) é 0
+// 1-3 Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
+// 1-4 Teste se a mensagem de erro é "parameters must be numbers" quando realizar chamada sum(4, "5")
+
 const assert = require('assert');
 
 function sum(a, b) {
@@ -8,6 +14,14 @@ function sum(a, b) {
   return a + b;
 }
 
-// implemente seus testes aqui
-const expected = sum (4, 5);
-assert.strictEqual(expected, 9 , 'A soma de 4 + 5 = 9 ');
+assert.strictEqual(typeof sum, 'function', 'O tipo de sum não é uma função');
+assert.strictEqual(sum(4,5),9, 'A soma de 4 + 5 = 9');
+assert.strictEqual(sum(0,0),0, 'A soma de 0 + 0 = 0');
+// assert.strictEqual(sum(4,'5'), 9, '5 é uma string');
+assert.throws(()=> {
+  sum(4,'5',);
+});
+
+assert.throws(()=> {
+  sum(4,'5',);
+},/^Error: parameters must be numbers$/);
